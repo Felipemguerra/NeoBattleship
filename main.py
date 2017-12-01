aa9f3a4d5e22d7e8ef698c45a7d4873f8b2573b3
Felipemguerra <felipeguerramail@gmail.com>
2017-12-01 17:41:34 -0500

    Added early peg framework

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 NBS_HEIGHT = 800/2
 NBS_WIDTH = 800/2
 
-#NBS_HEIGHT = NBS_HEIGHT
-#NBS_WIDTH = NBS_WIDTH
-
 class gameWindow(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
@@ -28,8 +25,8 @@ class gameWindow(QtWidgets.QMainWindow):
         menu_bar = self.menuBar()
         menu_bar.setNativeMenuBar(False)
         file_menu = menu_bar.addMenu("Options")
-        file_menu.addAction(exit_action)
-        
+        file_menu.addAction(exit_action)      
+
         self.show()
         
     def closeEvent(self, event):
@@ -40,115 +37,158 @@ class gameWindow(QtWidgets.QMainWindow):
             event.ignore()
             
 class gameGrid(QtWidgets.QWidget):
-    def __init__(self, parent):
-        QtWidgets.QWidget.__init__(self)
-        self.setup()
+	def __init__(self, parent):
+		QtWidgets.QWidget.__init__(self)
+		p = self.palette()
+		p.setColor(self.backgroundRole(), QtGui.QColor(150,177,210,255))
+		self.setPalette(p)
+		self.setAutoFillBackground(True)
+		self.setup()
 
-    def setup(self):
-        self.board = waterBoard(self)
-        self.enemyBoard = waterBoard(self)
-        #self.gameGrid = playGrid(self)
-        #self.new_btn = startNewGameBtn(self)
-        #self.quit_btn = quitBtn(self)
+	def setup(self):
+		self.board = Player(self)
+		self.enemyBoard = Computer(self)
+		#self.new_btn = startNewGameBtn(self)
+		#self.quit_btn = quitBtn(self)
+		self.grid = QtWidgets.QGridLayout()
+		self.setLayout(self.grid)
+		self.grid.addWidget(self.enemyBoard, 0,0,1,1)
+		self.grid.addWidget(self.board, 1,0,1,1)		
+		#self.grid.addWidget(self.new_btn, 3, 1, 1, 1)
+		#self.grid.addWidget(self.quit_btn, 3, 2, 1, 1)           
 
-        self.grid = QtWidgets.QGridLayout()
-        self.setLayout(self.grid)
+class Player(QtWidgets.QWidget):
+	def __init__(self, parent):
+		QtWidgets.QWidget.__init__(self, parent)
+		self.setFixedSize(NBS_WIDTH, NBS_HEIGHT)
+		self.grid = QtWidgets.QGridLayout()
+		self.setLayout(self.grid)
+		self.grid.setSpacing(0)
+		self.holes = []		
+		self.place_holes()
 
-        #self.grid.addWidget(self.gameGrid, 1, 1, 1, 4)
-        self.grid.addWidget(self.board, 2, 1, 1, 4)
-        self.grid.addWidget(self.enemyBoard, 1, 1, 1, 4)
-        #self.grid.addWidget(self.new_btn, 3, 1, 1, 1)
-        #self.grid.addWidget(self.quit_btn, 3, 2, 1, 1)           
+	def paintEvent(self, event):
+		qp = QtGui.QPainter()
+		qp.begin(self)
+		j = 10
+		dev = 10
+		for i in range(j):
+			qp.drawLine(QtCore.QLineF((NBS_WIDTH/j)*i+dev, 10, (NBS_WIDTH/j)*i+dev, NBS_HEIGHT-12))
+			qp.drawLine(QtCore.QLineF(10, (NBS_WIDTH/j)*i+dev, NBS_HEIGHT-12, (NBS_WIDTH/j)*i+dev))
+			dev +=2
+			
+		qp.end()
 
-class waterBoard(QtWidgets.QWidget):
+	def place_holes(self):
+		for i in range(9):
+			row = []
+			for e in range(9):
+				hole = playerHole(self)
+				self.grid.addWidget(hole,i,e)
+				row.append(hole)
+			self.holes.append(row[:])
 
-    def __init__(self, parent):
-        QtWidgets.QWidget.__init__(self, parent)
-        self.hole = []
-        """
-        Later on we can change this to new sizes based on screen aspect ratio and screen resolution
-        it will dynamically change all of the other sizes as well.
-        """
-        self.setFixedSize(NBS_WIDTH, NBS_HEIGHT)
-        p = self.palette()
-        p.setColor(self.backgroundRole(), QtGui.QColor(0,100,145,255))
-        self.setPalette(p)
-        self.setAutoFillBackground(True)
-        self.vbox = QtWidgets.QVBoxLayout()
-        self.vbox.setSpacing(0)
-        self.place_spots()
-        
+class Computer(QtWidgets.QWidget):
+	def __init__(self, parent):
+		QtWidgets.QWidget.__init__(self, parent)
+		self.setFixedSize(NBS_WIDTH, NBS_HEIGHT)
+		self.grid = QtWidgets.QGridLayout()
+		self.setLayout(self.grid)
+		self.grid.setSpacing(0)
+		self.holes = []		
+		self.place_holes()
 
-    def paintEvent(self, event):
-        points_list =  [QtCore.QPoint(10,10), QtCore.QPoint(10,NBS_HEIGHT-10),
-                        QtCore.QPoint(NBS_WIDTH-10,NBS_HEIGHT-10), QtCore.QPoint(NBS_WIDTH-10,10)]
-        square = QtGui.QPolygon(points_list)
-        #hline = QtCore.QLine(20,40,NBS_WIDTH-20,40)
-        #vline = QtCore.QLine(40,20,40,NBS_HEIGHT-20)  
+	def paintEvent(self, event):
+		qp = QtGui.QPainter()
+		qp.begin(self)
+		j = 10
+		dev = 10
+		for i in range(j):
+			qp.drawLine(QtCore.QLineF((NBS_WIDTH/j)*i+dev, 10, (NBS_WIDTH/j)*i+dev, NBS_HEIGHT-12))
+			qp.drawLine(QtCore.QLineF(10, (NBS_WIDTH/j)*i+dev, NBS_HEIGHT-12, (NBS_WIDTH/j)*i+dev))
+			dev +=2
+			
+		qp.end()
 
-        """
-        Here we are generating a pair of x,y coords to draw a line between
-        We're going to need two sets, one to draw horizontal lines, and one for vertical lines
-        
-        The gist of it here is that our range is the number of lines generated, with the 'length' of each line
-        being non-changing values over each loop so that it connects to both ends of our board
-        If we divide up our length into equal sections, then multiply that by i+1 so as to not draw extra lines
-        We will get evenly spaced lines each offset by +20 to align with our game board
-        """
+	def place_holes(self):
+		for i in range(9):
+			row = []
+			for e in range(9):
+				hole = computerHole(self)
+				self.grid.addWidget(hole,i,e)
+				row.append(hole)
+			self.holes.append(row[:])
 
-        j = 10
-        hlines = []
-        vlines = []
-        for i in range(j):
-            hline = QtCore.QLineF(20, (NBS_WIDTH/j)*i+20, NBS_HEIGHT-20, (NBS_WIDTH/j)*i+20)
-            vline = QtCore.QLineF((NBS_HEIGHT/j)*i+20, 20, (NBS_HEIGHT/j)*i+20, NBS_WIDTH-20)
-            hlines.append(hline) 
-            vlines.append(vline)
+class computerHole(QtWidgets.QWidget):
+	def __init__(self, parent):
+		QtWidgets.QWidget.__init__(self,parent)
+		self.peg = False
+		self.hit = False
+		self.x = 10
+		self.y = 10
 
-        qp = QtGui.QPainter()
-        qp.begin(self)
-        brush = QtGui.QBrush()
-        brush.setTextureImage(QtGui.QImage("water2_256.bmp"))
-        qp.setBrush(brush)
-        qp.drawPolygon(square)
-        qp.drawLines(hlines)
-        qp.drawLines(vlines) 
-        qp.end()
+	def paintEvent(self, event):
+		if self.peg == True and self.hit == True:
+			qp = QtGui.QPainter()
+			qp.begin(self)
+			brush = QtGui.QBrush(QtGui.QColor(255,0,0,255))
+			qp.setBrush(brush)
+			qp.drawEllipse(self.x,self.y,20,20)
+			qp.end()
+		elif self.peg == True and self.hit == False:
+			qp = QtGui.QPainter()
+			qp.begin(self)
+			brush = QtGui.QBrush(QtGui.QColor(0,255,0,255))
+			qp.setBrush(brush)
+			qp.drawEllipse(self.x,self.y,20,20)
+			qp.end()
+		else:
+			qp = QtGui.QPainter()
+			qp.begin(self)
+			brush = QtGui.QBrush(QtGui.QColor(0,0,0,255))
+			qp.setBrush(brush)
+			qp.drawEllipse(self.x,self.y,20,20)
+			qp.end()
 
+	def mousePressEvent(self, event):
+		if self.peg == False:
+			self.peg = True
+			self.update()
 
-    def place_spots(self):
-        for row in range(5):
-            row_list = []
-            rowLayout = QtWidgets.QHBoxLayout()
-            self.vbox.addLayout(rowLayout)
-            rowLayout.addStretch(1)
-            for col in range(5):
-                spot = gridSpot(self)
-                rowLayout.addWidget(spot,0)
-            rowLayout.addStretch(1)
+class playerHole(QtWidgets.QWidget):
+	def __init__(self, parent):
+		QtWidgets.QWidget.__init__(self,parent)
+		self.peg = False
+		self.hit = False
+		self.x = 10
+		self.y = 10
 
+	def paintEvent(self, event):
+		if self.peg == True and self.hit == True:
+			qp = QtGui.QPainter()
+			qp.begin(self)
+			brush = QtGui.QBrush(QtGui.QColor(255,0,0,255))
+			qp.setBrush(brush)
+			qp.drawEllipse(self.x,self.y,20,20)
+			qp.end()
+		elif self.peg == True and self.hit == False:
+			qp = QtGui.QPainter()
+			qp.begin(self)
+			brush = QtGui.QBrush(QtGui.QColor(0,255,0,255))
+			qp.setBrush(brush)
+			qp.drawEllipse(self.x,self.y,20,20)
+			qp.end()
+		else:
+			qp = QtGui.QPainter()
+			qp.begin(self)
+			brush = QtGui.QBrush(QtGui.QColor(0,0,0,255))
+			qp.setBrush(brush)
+			qp.drawEllipse(self.x,self.y,20,20)
+			qp.end()
 
-class gridSpot(QtWidgets.QWidget):
-    def __init__(self, parent):
-        QtWidgets.QWidget.__init__(self, parent)
-        self.grid = QtWidgets.QGridLayout()
-        self.setLayout(self.grid)
-        self.spot = None
-
-    def addPeg(self):
-        self.peg = Peg(self)
-        self.grid.addWidget(self.peg)
-
-    def paintEvent(self, event):
-        qp = QtGui.QPainter()
-        qp.begin(self) 
-        brush = QtGui.QBrush(QtCore.Qt.SolidPattern)
-        qp.setBrush(brush)
-        qp.drawEllipse(QtCore.QPointF(60,45), 6, 6)
-        qp.end()
-
-    def minimumSizeHint(self):
-        return QtCore.QSize(120,90)   
+	def addPeg(self):
+		self.peg = True
+		self.update()
 
 class ship(QtWidgets.QWidget):
     def __init__(self, parent):
